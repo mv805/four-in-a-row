1a3107706d6a0da642e0736127ac3c6725f18f72
Matt Villa <matthewvilla805@gmail.com>
2022-06-15 12:25:06 -0700

    Inactivated some unnecessary functions

--- src/player.js
+++ src/player.js
@@ -5,21 +5,21 @@ export const Player = (colorOfChecker) => {
     let playerChecker = Checker(colorOfChecker);
     let lastMove = undefined;
 
-    const getLastMove = () => {
-        return lastMove;
-    };
+    // const getLastMove = () => {
+    //     return lastMove;
+    // };
 
     const getCheckerElement = () => {
         return playerChecker.getElement();
     }
 
-    const setLastMove = (row, column) => {
-        lastMove = [row, column];
-    };
+    // const setLastMove = (row, column) => {
+    //     lastMove = [row, column];
+    // };
 
     return {
-        getLastMove,
-        setLastMove,
+        //getLastMove,
+        //setLastMove,
         getCheckerElement,
     };
 };
