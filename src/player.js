b5c604ff4a30098388deaa26bab33d4b1381e305
Matt Villa <matthewvilla805@gmail.com>
2022-06-15 15:18:47 -0700

    Removed comments

--- src/player.js
+++ src/player.js
@@ -5,21 +5,11 @@ export const Player = (colorOfChecker) => {
     let playerChecker = Checker(colorOfChecker);
     let lastMove = undefined;
 
-    // const getLastMove = () => {
-    //     return lastMove;
-    // };
-
     const getCheckerElement = () => {
         return playerChecker.getElement();
     }
 
-    // const setLastMove = (row, column) => {
-    //     lastMove = [row, column];
-    // };
-
     return {
-        //getLastMove,
-        //setLastMove,
         getCheckerElement,
     };
 };
