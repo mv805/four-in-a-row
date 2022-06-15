eefbdfbaeb98adc1e973c21df3990858507fd3f3
Matt Villa <matthewvilla805@gmail.com>
2022-06-15 08:34:20 -0700

    Players added

--- src/game-state.js
+++ src/game-state.js
@@ -1,3 +1,18 @@
-const gameState = (() => {
+import { Player } from '../src/player.js';
+
+export const GameState = (() => {
+
+    const playerYellow = Player('yellow');
+    const playerRed = Player('red');
+
+    let currentPlayer = playerRed;
+
+    const getCurrentPlayer = () => {
+        return currentPlayer;
+    };
+
+    return {
+        getCurrentPlayer,
+    };
 
 })();
