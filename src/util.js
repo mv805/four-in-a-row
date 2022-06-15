121ecad1545e3ba785d646ebc828889fa3c0eab0
Matt Villa <matthewvilla805@gmail.com>
2022-06-15 09:12:47 -0700

    removed function to add images to elements

--- src/util.js
+++ src/util.js
@@ -1,13 +1,3 @@
-export function toggleImageInElement(element, imageElement, imageOn) {
-
-    if (imageOn) {
-        element.appendChild(imageElement);
-    } else if (!imageOn) {
-        element.removeChild(imageElement);
-    }
-
-};
-
 export function removeAllChildNodes(parent) {
 
     while (parent.firstChild) {
