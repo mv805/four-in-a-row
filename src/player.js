54b56e70cc31d5e14acb4d0bb8806aaba81bbc3e
Matt Villa <matthewvilla805@gmail.com>
2022-06-15 16:42:32 -0700

    Get color function added

--- src/player.js
+++ src/player.js
@@ -3,13 +3,17 @@ import { Checker } from '../src/checker.js';
 export const Player = (colorOfChecker) => {
 
     let playerChecker = Checker(colorOfChecker);
-    let lastMove = undefined;
 
     const getCheckerElement = () => {
         return playerChecker.getElement();
-    }
+    };
+
+    const getPlayerColor = () => {
+        return colorOfChecker;
+    };
 
     return {
         getCheckerElement,
+        getPlayerColor,
     };
 };
